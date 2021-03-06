b92ad3f3f86aee8f1bf8abd8ecd7c0ae5f6fa496
Sanel Hadzini <sanel.hadzini@gmail.com>
2021-03-06 21:21:50 +0100

    code reorganization

---
+++ bot.py
@@ -0,0 +1,39 @@
+import nltk
+import stemmer
+import numpy
+import random
+import json
+
+with open("samples.json") as file:
+    data = json.load(file)
+
+def bagOfWords(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+
+def chat(model, payload):
+    words, labels = payload
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bagOfWords(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        print(random.choice(responses))
